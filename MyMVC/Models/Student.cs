b6b1ecb95165fdc6e8feffc3699153d5eec49190
БахытжановА <@206-4>
2024-01-24 19:11:06 +0600

    Интеграция с MyMVC

---
+++ MyMVC/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿namespace MyMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Result
+    {
+        public string result { get; set; }
+        public string error { get; set; }
+        public object model { get; set; }
+        public StatusEnum status { get; set; }
+    }
+
+    public enum StatusEnum
+    {
+        OK = 1,
+        ERROR = 0,
+        CriticalError = 2
+    }
+
+}
