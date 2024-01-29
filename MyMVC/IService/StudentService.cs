3d17d9a7f6236e80491e12853d88818ac6f29ec3
БахытжановА <@206-4>
2024-01-29 19:26:39 +0600

    допилил pStudentAdd

--- MyMVC/IService/StudentService.cs
+++ MyMVC/IService/StudentService.cs
@@ -26,6 +26,28 @@ namespace MyMVC.IService
             }
         }
 
-        public Result StudentAdd(Student model) { throw new NotImplementedException(); }
+        public Result StudentAdd(Student model) 
+        {
+            
+            using (SqlConnection db = new SqlConnection(constr))
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@firstname", $"{model.firstName}");
+                parameters.Add("@lastname", $"{model.lastName}");
+                parameters.Add("@dob", $"{model.birthDate}");
+                var res = db.Query<Student>("pStudentAdd", parameters, commandType: System.Data.CommandType.StoredProcedure);
+                Result result = new Result();
+                if (res != null) 
+                { 
+                    result.status = StatusEnum.OK;
+                }
+                else
+                {
+                    result.status = StatusEnum.ERROR;
+                }
+                return result;
+            }
+        
+        }
     }
 }
